0b44ab6fc062d2b26eca3e9e9200d418ee53de9a
Elijah Heisenberg <exijab@gmail.com>
2022-05-15 11:41:59 +0300

    Changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,54 +1,29 @@
-<svelte:head>
-	<title>Temp Title</title>
-	<meta name="description" content="" />
-</svelte:head>
-
 <script>
-	import '../app.css';
+  import "../app.css";
 </script>
 
-<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-	<div class="absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-  <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-2 ring-black rounded-sm drop-shadow-[3px_3px_0px_rgba(0,0,0,1)] sm:mx-auto sm:max-w-2xl sm:px-10">
-    <div class="mx-auto max-w-2xl">
-			<header class="sm:p-5 bg-sky-200 font-bold text-5xl text-sky-900 ring-2 ring-black rounded-sm drop-shadow-[3px_3px_0px_rgba(0,0,0,1)]">
-				<i class="bi bi-robot"></i>
-				Header
-			</header>
+<svelte:head>
+  <title>Temp Title</title>
+  <meta name="description" content="" />
+</svelte:head>
+
+<div class="relative flex flex-col justify-center min-h-screen overflow-hidden bg-gray-50 sm:py-6">
+  <div class="absolute inset-0 bg-[url(/circles.svg)]" />
+  <div class="relative bg-white shadow-xl ring-1 ring-gray-900 drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] sm:mx-auto sm:max-w-2xl">
+    <header class="text-5xl font-bold sm:mx-auto sm:max-w-2xl sm:p-5 bg-sky-200 text-sky-900 ring-1 ring-gray-900">
+      <i class="bi bi-robot" />
+      Header
+    </header>
+    <div class="max-w-2xl mx-auto sm:px-10">
       <div class="divide-y divide-gray-300/50">
-        <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
-
-					<slot />
-
-          <ul class="space-y-4">
-            <li class="flex items-center">
-							Testing, 1, 2...
-            </li>
-            <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="11" />
-                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-              </svg>
-              <p class="ml-4">
-                Extracting classes with
-                <code class="text-sm font-bold text-gray-900">@apply</code>
-              </p>
-            </li>
-            <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="11" />
-                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-              </svg>
-              <p class="ml-4">Code completion with instant preview</p>
-            </li>
-          </ul>
-          <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+        <div class="space-y-6 text-base leading-7 text-gray-600">
+          <slot />
         </div>
-				<footer class="sm:p-5 bg-sky-300 font-bold text-5xl text-sky-900 ring-2 ring-black rounded-sm drop-shadow-[3px_3px_0px_rgba(0,0,0,1)]">
-					<i class="bi bi-arrow-down"></i>
-					Footer
-				</footer>
       </div>
     </div>
+    <footer class="text-5xl font-bold sm:mx-auto sm:max-w-2xl sm:p-5 bg-sky-200 text-sky-900 ring-1 ring-gray-900">
+      <i class="bi bi-arrow-down" />
+      Feader
+    </footer>
   </div>
 </div>
